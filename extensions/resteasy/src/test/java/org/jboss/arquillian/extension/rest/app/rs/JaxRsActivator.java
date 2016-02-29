c1acfe366fb63a1f96a173cb1deb5a12991506be
Fabrizio Torelli <hellgate75@gmail.com>
2016-02-29 23:09:35 +0000

    Remove not needed extensions

--- extensions/resteasy/src/test/java/org/jboss/arquillian/extension/rest/app/rs/JaxRsActivator.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.arquillian.extension.rest.app.rs;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class JaxRsActivator extends Application {
-}
